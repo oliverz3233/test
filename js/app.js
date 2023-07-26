f41094e6244306bdb3cc7c2f9a60bd44338f5052
oliverz3233 <110497156+oliverz3233@users.noreply.github.com>
2023-07-26 15:00:06 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -20,23 +20,6 @@ var uploadButton = document.getElementbyId("uploadButton");
 recordButton.addEventListener("click", startRecording);
 stopButton.addEventListener("click", stopRecording);
 pauseButton.addEventListener("click", pauseRecording);
-uploadButton.addEventListener("submit", function(e) {
-	e.preventDefault();
-
-	const userFile = document.getElementbyId('file').files[0];
-	
-	const form = new FormData();
-	form.append('user-file', userFile, 'user-file.jpg');
-	
-	fetch('https://ingestion.edgeimpulse.com/api/training/anomaly', {
-	method: 'POST',
-	headers: {
-	  'x-api-key': 'ei_5cf10097b6661fc9e5067e0934358cd93bf5a4c7e839e08087aad2e571c2e4ef',
-	  'Content-Type': 'multipart/form-data'
-	},
-	body: form
-	});
-});
 
 function startRecording() {
 	console.log("recordButton clicked");
