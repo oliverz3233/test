b9a15bcbc096998614d840c23113233b2ccd5807
oliverz3233 <110497156+oliverz3233@users.noreply.github.com>
2023-07-26 14:18:28 -0700

    Update API.js

--- edge_impulse/API.js
+++ edge_impulse/API.js
@@ -1,1 +1,15 @@
+function upload(filename) {
+  import fetch, { FormData, fileFromSync } from 'node-fetch';
 
+  const form = new FormData();
+  form.append('data', fileFromSync(filename + '.wav'));
+  
+  fetch('https://ingestion.edgeimpulse.com/api/training/anomaly', {
+      method: 'POST',
+      headers: {
+          'x-api-key': 'ei_5cf10097b6661fc9e5067e0934358cd93bf5a4c7e839e08087aad2e571c2e4ef',
+          'Content-Type': 'multipart/form-data'
+      },
+      body: form
+  });
+}
