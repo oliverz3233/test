95ee980e57735f487767570f3fd8994b89372fb7
oliverz3233 <110497156+oliverz3233@users.noreply.github.com>
2023-07-26 15:01:16 -0700

    Update API.js

--- edge_impulse/API.js
+++ edge_impulse/API.js
@@ -1,15 +1,19 @@
-function upload(filename) {
-  import fetch, { FormData, fileFromSync } from 'node-fetch';
+import fetch, { FormData, fileFromSync } from 'node-fetch';
 
-  const form = new FormData();
-  form.append('data', fileFromSync(filename + '.wav'));
-  
-  fetch('https://ingestion.edgeimpulse.com/api/training/anomaly', {
-      method: 'POST',
-      headers: {
-          'x-api-key': 'ei_5cf10097b6661fc9e5067e0934358cd93bf5a4c7e839e08087aad2e571c2e4ef',
-          'Content-Type': 'multipart/form-data'
-      },
-      body: form
-  });
-}
+uploadButton.addEventListener("submit", function(e) {
+	e.preventDefault();
+
+	const userFile = document.getElementbyId('file').files[0];
+	
+	const form = new FormData();
+	form.append('user-file', userFile, 'user-file.jpg');
+	
+	fetch('https://ingestion.edgeimpulse.com/api/training/anomaly', {
+	method: 'POST',
+	headers: {
+	  'x-api-key': 'ei_5cf10097b6661fc9e5067e0934358cd93bf5a4c7e839e08087aad2e571c2e4ef',
+	  'Content-Type': 'multipart/form-data'
+	},
+	body: form
+	});
+});
