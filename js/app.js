5b3aba8145131eee057f3ad2e38fb9c2307aa7a6
oliverz3233 <110497156+oliverz3233@users.noreply.github.com>
2023-08-02 17:47:02 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -20,6 +20,10 @@ pauseButton.addEventListener("click", pauseRecording);
 
 var interval;
 
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
+document.getElementsByTagName('head')[0].appendChild(script);
+
 function startRecording() {
 	console.log("recordButton clicked");
 
